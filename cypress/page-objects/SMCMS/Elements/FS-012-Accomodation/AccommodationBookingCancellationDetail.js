9079c2dc1f5786a3ee249df8b5db6221009ca3bd
Fawad Shahzada <FShahzada@synergyonline.com>
2023-04-18 14:33:53 +0500

    added booking cancellation

--- cypress/page-objects/SMCMS/Elements/FS-012-Accomodation/AccommodationBookingCancellationDetail.js
+++ cypress/page-objects/SMCMS/Elements/FS-012-Accomodation/AccommodationBookingCancellationDetail.js
@@ -1,31 +1,29 @@
 class elems_AccommodationBookingCancellationDetail {
 
-    static BTN_CREATENEW = '//button[text()="Create New"]'
+    static BTN_SUBMIT = '//button[text()="Submit"]'
+    static BTN_CANCEL = '//button[text()="Cancel"]'
 
     static TXT_CANCELLATIONNUMBER = '//input[@id="txtCancellationNumber"]'
-    
-    static PCK_MAINACCOMMODATION = '//label[text()="Main Accommodation"]//following-sibling::div//button'
-    static TXT_ACCOMMODATION = '//input[@name="name"]'
-    static BTN_SEARCHFILTERACC = '(//button[text()="Search Filters"])[1]'
-    
-    static DATE_CHECKINFROM = '//input[@id="dtpStartDateFrom"]'
-    static DATE_CHECKINTO = '//input[@id="dtpStartDateTo"]'
 
-    static DATE_CHECKINOUTFROM = '//input[@id="dtpEndDateFrom"]'
-    static DATE_CHECKINOUTTO = '//input[@id="dtpStartDateTo"]'
-
-    static TXT_CUSTOMERNRIC = '//input[@id="txtCustomerNRIC"]'
-    static TXT_CUSTOMERNAME = '//input[@id="txtCustomerName"]'
-
-    static PCK_CUSTOMERCATEGORY = '//label[text()="Customer Category"]//following-sibling::div//button'
-    static TXT_CUSTOMERCATEGORYNAME = '//input[@id="txtCustomerCategoryName"]'
-    static TXT_CUSTOMERCATEGORYNUMBER = '//input[@id="txtCustomerCategoryNumber"]'
     
-    static DRP_STATUS = '//span[@id="drpStatus"]'
-    static DRP_RECORDSTATUS = '//span[@id="drpRecordStatus"]'
+    static PCK_ACCOMMODATIONBOOKING = '//label[text()="Accommodation Booking"]//following-sibling::div//button'
+    static TXT_BOOKINGNUMBER = '//input[@id="bookingNumber"]'
+    static TXT_CUSTOMERNAME = '//input[@name="customerName"]'
+    static BTN_SEARCHFILTERS = '//button[text()="Search Filters"]'
+
+    static LBL_MAINACCOMMODATION = '//label[@for="lblMainFacility"]'
+    static LBL_MEMBERID = '//label[@for="lblMemberID"]'
+    static LBL_CUSTOMERNAME = '//label[@for="lblCustomerName"]'
+
+    //Payment detail
+
+    static CHK_CALCULATEPAYMENT = '//input[@id="chkCalculatePayment"]'
+
+    static CHK_GIVEREFUNDCRIDT = '//input[@id="chkGiveRefundCredit"]'
+    static DRP_REFUNDCRIDTTYPE = '//span[@id="drpRefundCreditType"]'
+    static DRP_AMOUNTTYPE = '//span[@id="drpAmountType"]'
+    static TXTAERA_ = '//textarea[@id="txtRefundCreditReason"]'
     
-    static BTN_SEARCHFILTER = '//button[text()="Search Filters"]'
-    static TBL_ACCOMADATIONBOOKINGLISTING = '//h2[text()="Accommodation Booking Cancellation Listing"]//ancestor::div//table'
     
 }
 export default elems_AccommodationBookingCancellationDetail
