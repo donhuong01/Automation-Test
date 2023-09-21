1a4b477fbdec7d3d33cdd7064832f814ce968a57
Julius <71910804+mithx1@users.noreply.github.com>
2023-09-21 10:45:02 +0800

    format

--- cypress/page-objects/SMCMS/PageActions/FS-004-Admin-Settings/FS-004-Customer Category/CustomerCategoryListing.js
+++ cypress/page-objects/SMCMS/PageActions/FS-004-Admin-Settings/FS-004-Customer Category/CustomerCategoryListing.js
@@ -8,46 +8,44 @@ class CustomerCategoryListingForm {
      * Description: This function Verifies the Form Title
      *
      *****************************************************/
-     VerifyPageTitle()
-     {
-        cy.VerifyElementText(elems_PageHeader.LBL_PAGETITLE,"Customer Category Listing")
+    VerifyPageTitle() {
+        cy.VerifyElementText(elems_PageHeader.LBL_PAGETITLE, "Customer Category Listing")
 
-     }
+    }
 
     /*****************************************************
      * Method: Filters Functionality
-     * Description: This function Verifies the fiter functionality 
+     * Description: This function Verifies the fiter functionality
      * @param {string} CustomerCategoryName catergory name
      * @param {string} CustomerCategoryNumber customer caterogy number
-     * @param {string} CustomerCatergoryType customer category type  
+     * @param {string} CustomerCatergoryType customer category type
      *****************************************************/
-    
-    FillOutFilters(CustomerCategoryName,CustomerCategoryNumber,CustomerCategoryType,D365ID)
-    {
-        if(CustomerCategoryName !== undefined){
+
+    FillOutFilters(CustomerCategoryName, CustomerCategoryNumber, CustomerCategoryType, D365ID) {
+        if (CustomerCategoryName !== undefined) {
             cy.xpath(elems_CustomerCategoryListing.TXT_CUSTOMERCATEGORYNAME).clear()
             cy.EnterText(elems_CustomerCategoryListing.TXT_CUSTOMERCATEGORYNAME, CustomerCategoryName)
         }
-        if(CustomerCategoryNumber !==undefined){
+        if (CustomerCategoryNumber !== undefined) {
             cy.xpath(elems_CustomerCategoryListing.TXT_CUSTOMERCATEGORYNUMBER).clear()
-        cy.EnterText(elems_CustomerCategoryListing.TXT_CUSTOMERCATEGORYNUMBER, CustomerCategoryNumber)
+            cy.EnterText(elems_CustomerCategoryListing.TXT_CUSTOMERCATEGORYNUMBER, CustomerCategoryNumber)
         }
-        if(CustomerCategoryType !==undefined){
+        if (CustomerCategoryType !== undefined) {
             cy.xpath(elems_CustomerCategoryListing.TXT_CUSTOMERCATEGORYTYPE).clear()
-        cy.EnterText(elems_CustomerCategoryListing.TXT_CUSTOMERCATEGORYTYPE, CustomerCategoryType)
+            cy.EnterText(elems_CustomerCategoryListing.TXT_CUSTOMERCATEGORYTYPE, CustomerCategoryType)
         }
-    //    if(CustomerCategoryName === undefined
-    //     && CustomerCategoryNumber ===undefined
-    //     && CustomerCategoryType === undefined
-    //     && D365ID)
-    //     {
-    //         throw new Error("SourceChannelListing.Fillout Error!!./n\
-    //                         Please provide at least one argument.")
-    //     }
-    cy.Click(elems_CustomerCategoryListing.BTN_SEARCH)
-    cy.wait(7000)
+        //    if(CustomerCategoryName === undefined
+        //     && CustomerCategoryNumber ===undefined
+        //     && CustomerCategoryType === undefined
+        //     && D365ID)
+        //     {
+        //         throw new Error("SourceChannelListing.Fillout Error!!./n\
+        //                         Please provide at least one argument.")
+        //     }
+        cy.Click(elems_CustomerCategoryListing.BTN_SEARCH)
+        cy.wait(7000)
     }
-    FilterAndClickItem(CustomerCategoryName){
+    FilterAndClickItem(CustomerCategoryName) {
         cy.xpath(elems_CustomerCategoryListing.TXT_CUSTOMERCATEGORYNAME).clear()
         cy.EnterText(elems_CustomerCategoryListing.TXT_CUSTOMERCATEGORYNAME, CustomerCategoryName)
         cy.Click(elems_CustomerCategoryListing.BTN_SEARCH)
@@ -59,33 +57,35 @@ class CustomerCategoryListingForm {
      * Method:  ClickDelete
      * Description: This function Click on Delete Button
      *****************************************************/
-    ClickDelete(CustomerCategoryName,D365ID){
-            // cy.SelectTableItem(
-            // elems_CustomerCategoryListing.TBL_CUSTOMERCATEGORYLISTING, 
-            // 'Customer Category Name', CustomerCategoryName,
-            // 'D365 ID', D365ID)
-             cy.Click(elems_CustomerCategoryListing.BTN_DELETE)
-            // }
-    
-            // ClickDeleteInDeleteConfirmationPopup()
+    ClickDelete(CustomerCategoryName, D365ID) {
+        // cy.SelectTableItem(
+        // elems_CustomerCategoryListing.TBL_CUSTOMERCATEGORYLISTING,
+        // 'Customer Category Name', CustomerCategoryName,
+        // 'D365 ID', D365ID)
+        cy.Click(elems_CustomerCategoryListing.BTN_DELETE)
+        // }
+
+        // ClickDeleteInDeleteConfirmationPopup()
         // {
-            cy.Click(elems_Alerts.BTN_DELETEPOPUP)
-        }
-    
+        cy.Click(elems_Alerts.BTN_DELETEPOPUP)
+    }
+
     /*****************************************************
      * Method: CreateNew
      * Description: This function click on Create New Button
      *****************************************************/
-    CreateNew(){
-        cy.Click(elems_CustomerCategoryListing.BTN_CREATENEW)
+    CreateNew() {
+        cy.get('.k-loading-image', { timeout: 30000 }).should('not.exist');
+        cy.xpath(elems_CustomerCategoryListing.BTN_CREATENEW, { timeout: 10000 }) // Adjust the timeout value as needed
+            .should('be.visible') // Ensure the element is visible
+            .click(); // Click the element
     }
 
     /*****************************************************
      * Method: ClickCancelInDeleteConfirmationPopup
      * Description: This function click on Cancel button in delete confirmation Popup
      *****************************************************/
-    ClickCancelInDeleteConfirmationPopup()
-    {
+    ClickCancelInDeleteConfirmationPopup() {
         cy.Click(elems_Alerts.BTN_Cancel)
     }
 
@@ -93,52 +93,48 @@ class CustomerCategoryListingForm {
      * Method: ClickDeleteInDeleteConfirmationPopup
      * Description: This function click on Delete button in delete confirmation Popup
      *****************************************************/
-    ClickDeleteInDeleteConfirmationPopup()
-    {
+    ClickDeleteInDeleteConfirmationPopup() {
         cy.Click(elems_Alerts.BTN_DELETEPOPUP)
     }
 
     /*****************************************************
      * Method: Validate deleted item in the list
      * Description: This function Verifies the deleted item in the list
-     * 
-     * @param {string} CategoryName 
-     * @param {string} D365ID  
+     *
+     * @param {string} CategoryName
+     * @param {string} D365ID
      *****************************************************/
-    ValidateDeletedItem(CategoryName,D365ID)
-    {
-        if(cy.SelectTableItem(
-            elems_CustomerCategoryListing.TBL_CUSTOMERCATEGORYLISTING, 
+    ValidateDeletedItem(CategoryName, D365ID) {
+        if (cy.SelectTableItem(
+            elems_CustomerCategoryListing.TBL_CUSTOMERCATEGORYLISTING,
             'Customer Category Name', CategoryName,
-            'D365 ID', D365ID))
-         {
+            'D365 ID', D365ID)) {
             cy.log("ERROR DELETING THE ITEM")
-         }  
-         else
-         {
+        }
+        else {
             cy.log("ITEM IS DELETED")
-            cy.VerifyElementText(elems_Alerts.DELETE_NOTIFICATION,'Record has been deleted successfully.')
-         }
-        
+            cy.VerifyElementText(elems_Alerts.DELETE_NOTIFICATION, 'Record has been deleted successfully.')
+        }
+
 
     }
     /*****************************************************
      * Method: UpdatingItem
      * Description: This function is to update data
-     * 
+     *
      * @param {string} CustomerCategoryName
-     * @param {string} D365ID  
+     * @param {string} D365ID
      *****************************************************/
 
-    UpdatingItem(CustomerCategoryName, D365ID){
+    UpdatingItem(CustomerCategoryName, D365ID) {
 
         // cy.ClickTableLink(
-        //     elems_CustomerCategoryListing.TBL_CUSTOMERCATEGORYLISTING , 
+        //     elems_CustomerCategoryListing.TBL_CUSTOMERCATEGORYLISTING ,
         //     'Customer Category Name', CustomerCategoryName,
         //     'D365 ID', D365ID)
-        
+
         cy.Click(`//table[@role="presentation"]//a[text()="${CustomerCategoryName}"]`)
-         }
+    }
 
 }
 
