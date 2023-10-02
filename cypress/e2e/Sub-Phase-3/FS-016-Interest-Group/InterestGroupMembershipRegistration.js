08043e411cc4e609ff29ab29379d26bdfc35f93b
Julius <71910804+mithx1@users.noreply.github.com>
2023-10-02 15:59:03 +0800

    USAGE: nricGenerator('S',age)

--- cypress/e2e/Sub-Phase-3/FS-016-Interest-Group/InterestGroupMembershipRegistration.js
+++ cypress/e2e/Sub-Phase-3/FS-016-Interest-Group/InterestGroupMembershipRegistration.js
@@ -11,6 +11,7 @@ import Customerdata from '../../../fixtures/Data_Module/CustomerCreationData'
 import elems_CustomerCheckInPage from '../../../page-objects/SMCMS/Elements/Membership/FS014_Membership-Master-Registration-Renewal/CustomerCheckInPage'
 import IGMembershipRegistration from '../../../page-objects/SMCMS/PageActions/FS-016-Interest-Group/Interest Group Membership Registarion'
 import IGRegistration from '../../../fixtures/Data_Module/FS-016-Interest-Group/FS-016-Interest-Group-Management'
+import { nricGenerator } from '../../../support/nricGenerator'
 
 //Page definition
 const MemRegPrincipal = new MemberRegistrationPrincipal()
@@ -36,62 +37,10 @@ const { IGMainSelectionBox, IGSelection, IGLocationSelection, MembershipTenure, 
 //     // cy.SaveUserInfoInLocalStorageForUAT(login.authenticated_user_uat, login.active_location_uat, login.safra_client_uat)
 // })
 
-//TODO to separate AutoGenerateNRIC in a separate file
-//#region AutoGenerateNRIC
-function pad(num, size) {
-    var s = "0000000" + num;
-    return s.substr(s.length - size);
-}
-
-// first = S,T,F,G age=10-100
-function generate(first, age) {
-    age = parseInt(age, 10);
-    if (first != 'S' && first != 'T' && first != 'F' && first != 'G') return;
-    if (!(age >= -1 && age <= 9)) age = -1;
-
-    // The IC Generator Algorithm
-    var chars = pad(Math.floor(Math.random() * 9999999), 7).split('');
-    if (age != -1) chars[0] = age;
-
-    var output = first + chars.join('');
-
-    chars[0] *= 2;
-    chars[1] *= 7;
-    chars[2] *= 6;
-    chars[3] *= 5;
-    chars[4] *= 4;
-    chars[5] *= 3;
-    chars[6] *= 2;
-
-    var sum = 0;
-    var offset = (first == "T" || first == "G") ? 4 : 0;
-    var st = ["J", "Z", "I", "H", "G", "F", "E", "D", "C", "B", "A"];
-    var fg = ["X", "W", "U", "T", "R", "Q", "P", "N", "M", "L", "K"];
-    for (var i = 0; i <= 6; i++) {
-        sum += chars[i];
-    }
-    var temp = (offset + sum) % 11;
-
-    var theAlpha = "";
-    if (first == "S" || first == "T") {
-        theAlpha = st[temp];
-    } else if (first == "F" || first == "G") {
-        theAlpha = fg[temp];
-    } else theAlpha = "?";
-
-    return output + theAlpha;
-}
-
-const nricGenerator = () => {
-    const generated = generate("S", 40);
-    console.log("Generated NRIC:", generated); // Add this line for debugging
-    return generated;
-};
-//#endregion
 
 const InterestGroupMembershipRegistration = () => {
 
-    const CustomerNRICFull = nricGenerator()
+    const CustomerNRICFull = nricGenerator('S', 43)
 
     const CustomerNRIC = CustomerNRICFull.substr(CustomerNRICFull.length - 4);
 
